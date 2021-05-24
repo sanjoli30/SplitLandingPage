6ee32faf27ab368dbf9eb54320e91874426933c1
sanjoli30 <sanjolisjain@gmail.com>
2021-05-24 19:15:55 +0530

    Split Landing Webpage Complete

---
+++ script.js
@@ -0,0 +1,30 @@
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
+
+left.addEventListener('mouseenter', () => {
+        container.classList.add('hover-left');
+})
+
+left.addEventListener('mouseleave', () => {
+        container.classList.remove('hover-left');
+})
+
+right.addEventListener('mouseenter', () => {
+        container.classList.add('hover-right');
+})
+
+right.addEventListener('mouseleave', () => {
+        container.classList.remove('hover-right');
+})
+
+// left.addEventListener('mouseenter', () => {
+//     container.classList.remove('hover-right');
+//     container.classList.add('hover-left');
+// })
+
+// right.addEventListener('mouseenter', () => {
+//     container.classList.remove('hover-left');
+//     container.classList.add('hover-right');
+// })
+
